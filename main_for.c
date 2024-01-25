1c37f4ff14cc93d33bac9017909c35872a272897
Paulo <vanderley.1109@gmail.com>
2024-01-25 11:19:56 -0300

    atualizando conteudo

--- main_for.c
+++ main_for.c
@@ -1,11 +1,17 @@
+// Inclui bibliotecas necessárias.
 #include <stdio.h>
 #include <stdlib.h>
 
+// Função principal.
 void main()
 {
+    // Loop que itera de 0 a 10 (11 iterações).
     for (int l = 0; l < 11; l++)
     {
+        // Imprime o valor de 'l' e a mensagem "TreinaWeb!".
         printf("%d TreinaWeb!\n", l);
     }
+
+    // Retorna 0 indicando que a execução ocorreu sem erros.
     return 0;
 }
